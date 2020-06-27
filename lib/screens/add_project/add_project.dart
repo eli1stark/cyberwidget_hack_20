df7e7d483c872aee67cd84aeb9984946fe676fa9
Hashem <hashemp206@yahoo.com>
2020-06-27 19:04:30 +0430

    Add profile_settings screen

--- lib/screens/add_project/add_project.dart
+++ lib/screens/add_project/add_project.dart
@@ -18,6 +18,129 @@ class AddProject extends StatelessWidget {
           onTapLeft: () {},
         ),
         preferredSize: Size.fromHeight(kToolbarHeight),
+<<<<<<< HEAD
+      ),
+      body: Wrap(
+        direction: Axis.vertical,
+        children: <Widget>[
+          TextField(
+            cursorColor: Colors.white,
+            style: TextStyle(color: Colors.white),
+            decoration: InputDecoration(
+              hintText: 'Title',
+              hintStyle: TextStyle(color: Colors.white),
+              //border: OutlineInputBorder(borderSide: BorderSide(color: Colors.white))
+            ),
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          Text(
+            "Description",
+            style: TextStyle(color: Colors.white, fontSize: 24),
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          TextField(
+            cursorColor: Colors.white,
+            style: TextStyle(color: Colors.white),
+            decoration: InputDecoration(
+              hintText: 'Write Your Description Here!',
+              hintStyle: TextStyle(color: Colors.white),
+              //border: OutlineInputBorder(borderSide: BorderSide(color: Colors.white))
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                "Tags",
+                style: TextStyle(color: Colors.white, fontSize: 24),
+              ),
+              TextField(
+                cursorColor: Colors.white,
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  hintText: '',
+                  hintStyle: TextStyle(color: Colors.white),
+                  //border: OutlineInputBorder(borderSide: BorderSide(color: Colors.white))
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                "Repo Link",
+                style: TextStyle(color: Colors.white, fontSize: 24),
+              ),
+              TextField(
+                cursorColor: Colors.white,
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  hintText: 'www.github.com',
+                  hintStyle: TextStyle(color: Colors.white),
+                  //border: OutlineInputBorder(borderSide: BorderSide(color: Colors.white))
+                ),
+              ),
+            ],
+          ),
+          Stack(
+            children: <Widget>[
+              Container(
+                height: 130,
+                width: 354,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(44),
+                ),
+                color: Colors.white,
+                child: Image(image: null),
+              ),
+              FlatButton(
+                child: Container(
+                    height: 60,
+                    width: 200,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(18),
+                    ),
+                    color: Color(0xff434343),
+                    child: Center(
+                      child: Text(
+                        "Repo Link",
+                        style: TextStyle(color: Colors.white, fontSize: 24),
+                      ),
+                    )),
+                onPressed: () {},
+              ),
+            ],
+          ),
+          Row(
+             children: <Widget>[
+               Container(
+                height: 80,
+                width: 80,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                color: Colors.white,
+                child: Image(image: null),
+              ),
+              Container(
+                height: 80,
+                width: 80,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(44),
+                ),
+                color: Colors.white,
+                child: Icon(Icons.add_a_photo),
+              ),
+             ],
+          )
+        ],
+=======
+>>>>>>> 2171eb492cb25b0a11a44e4560e2b92a47585f42
       ),
     );
   }
--- lib/screens/profile_settings/profile_settings.dart
+++ lib/screens/profile_settings/profile_settings.dart
@@ -2,12 +2,191 @@ import 'package:flutter/material.dart';
 
 class ProfileSettings extends StatelessWidget {
   static const routeName = '/profile_settings';
+  static const double horizontalPadding = 30;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Cart'),
+        title: Text('Edit Profile'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.check),
+            onPressed: () {},
+          )
+        ],
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: horizontalPadding),
+              child: Text(
+                'Choose Your Character',
+                style: TextStyle(
+                    fontSize: 21,
+                    color: Theme.of(context).primaryTextTheme.button.color),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              height: 130,
+              child: ListView.builder(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: horizontalPadding),
+                scrollDirection: Axis.horizontal,
+                itemCount: 10,
+                itemBuilder: (BuildContext context, int index) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 5),
+                    child: Container(
+                      width: 90,
+                      height: 130,
+                      decoration: BoxDecoration(
+                          color: Colors.amber,
+                          border:
+                              Border.all(color: Theme.of(context).accentColor),
+                          borderRadius: BorderRadius.circular(20)),
+                    ),
+                  );
+                },
+              ),
+            ),
+            const SizedBox(
+              height: 46,
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: horizontalPadding),
+              child: Text(
+                'Choose Your Background',
+                style: TextStyle(
+                    fontSize: 21,
+                    color: Theme.of(context).primaryTextTheme.button.color),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              height: 122,
+              child: ListView.builder(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: horizontalPadding),
+                scrollDirection: Axis.horizontal,
+                itemCount: 10,
+                itemBuilder: (BuildContext context, int index) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Container(
+                      width: 180,
+                      height: 122,
+                      decoration: BoxDecoration(
+                          color: Colors.amber,
+                          border:
+                              Border.all(color: Theme.of(context).accentColor),
+                          borderRadius: BorderRadius.circular(20)),
+                    ),
+                  );
+                },
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: horizontalPadding),
+              child: Row(
+                children: <Widget>[
+                  Flexible(
+                    flex: 1,
+                    child: ProfileTextField(title: 'First Name'),
+                  ),
+                  SizedBox(
+                    width: 40,
+                  ),
+                  Flexible(
+                    flex: 1,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text('Last Name',
+                            style: TextStyle(
+                                fontSize: 21,
+                                color: Theme.of(context)
+                                    .primaryTextTheme
+                                    .button
+                                    .color)),
+                        const SizedBox(
+                          height: 17,
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                            enabledBorder: const OutlineInputBorder(
+                              borderRadius: const BorderRadius.all(
+                                  const Radius.circular(20.0)),
+                              borderSide:
+                                  BorderSide(width: 2, color: Colors.white),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: const BorderRadius.all(
+                                  const Radius.circular(20.0)),
+                              borderSide: BorderSide(
+                                  width: 2,
+                                  color: Theme.of(context).accentColor),
+                            ),
+                          ),
+                          cursorColor: Colors.white,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ProfileTextField extends StatelessWidget {
+  const ProfileTextField({String title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text('First Name',
+            style: TextStyle(
+                fontSize: 21,
+                color: Theme.of(context).primaryTextTheme.button.color)),
+        const SizedBox(
+          height: 17,
+        ),
+        TextField(
+          decoration: InputDecoration(
+            enabledBorder: const OutlineInputBorder(
+              borderRadius: const BorderRadius.all(const Radius.circular(20.0)),
+              borderSide: BorderSide(width: 2, color: Colors.white),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: const BorderRadius.all(const Radius.circular(20.0)),
+              borderSide:
+                  BorderSide(width: 2, color: Theme.of(context).accentColor),
+            ),
+          ),
+          cursorColor: Colors.white,
+          style: TextStyle(color: Colors.white),
+        ),
+      ],
     );
   }
 }
