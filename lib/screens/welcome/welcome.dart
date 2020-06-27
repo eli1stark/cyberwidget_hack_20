485220d7652469fecd7ab7f2523bc82c5166f1ef
Balaji <vbalaji165@gmail.com>
2020-06-27 12:43:58 +0530

    Updated welcome screens

--- lib/screens/welcome/welcome.dart
+++ lib/screens/welcome/welcome.dart
@@ -1,3 +1,5 @@
+import 'package:cyberwidget_hack_20/screens/welcome/sigin.dart';
+import 'package:cyberwidget_hack_20/screens/welcome/signup.dart';
 import 'package:flutter/material.dart';
 
 class Welcome extends StatelessWidget {
@@ -26,21 +28,21 @@ class Welcome extends StatelessWidget {
             Container(
               width: width,
               height: height,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [
-                    Color.fromRGBO(241, 0, 156, 0.38),
-                    Color(0xff1A053E)
-                  ]
-                )
-              ),
+              child: Image.asset('assets/design.png',fit: BoxFit.fill,),
             ),
             Container(
               width: width,
               height: height,
-              child: Image.asset('assets/design.png',fit: BoxFit.fill,),
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        Color.fromRGBO(241, 0, 156, 0.38),
+                        Color(0xff1A053E)
+                      ]
+                  )
+              ),
             ),
             Align(
               alignment: Alignment.center,
@@ -56,11 +58,9 @@ class Welcome extends StatelessWidget {
                   Container(
                     width: width*0.65,
                     height: 50.0,
-                    child: Expanded( //here is the expandable text
-                      child: Text('Discover the top Flutter Designers & Developers',style: TextStyle(
-                        fontSize: 15.0,color: Color(0xffC4C4C4),
-                      ),),
-                    ),
+                    child: Text('Discover the top Flutter Designers & Developers',style: TextStyle(
+                      fontSize: 15.0,color: Color(0xffC4C4C4),
+                    ),),
                   ),
                 ],
               ),
@@ -74,9 +74,18 @@ class Welcome extends StatelessWidget {
                   children: [
                     Container(
                       width: width*0.7,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12.0),
+                        boxShadow:[BoxShadow(
+                          color: Color(0xff6F29E6),
+                          spreadRadius: 2.0,
+                          blurRadius: 4.0,
+                          offset: Offset(0,4),
+                        )]
+                      ),
                       child: RaisedButton(
                         shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10.0),
+                          borderRadius: BorderRadius.circular(12.0),
                         ),
                         onPressed: (){
 
@@ -107,36 +116,57 @@ class Welcome extends StatelessWidget {
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
-                          RaisedButton(
-                            color: Color(0xffF1009C),
-                            onPressed: (){
-
-
-                            },
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10.0)
+                          Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(12.0),
+                                boxShadow:[BoxShadow(
+                                  color: Color(0xff6F29E6),
+                                  spreadRadius: 1.0,
+                                  blurRadius: 8.0,
+                                  offset: Offset(4,4),
+                                )]
                             ),
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Text('Sign in',style: TextStyle(
-                                fontSize: 20.0,color: Color(0xffC4C4C4),
+                            child: RaisedButton(
+                              color: Color(0xffF1009C),
+                              onPressed: (){
+                                Navigator.push(context, MaterialPageRoute(builder: (context)=>Signup()));
+                              },
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10.0)
                               ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text('Sign in',style: TextStyle(
+                                  fontSize: 20.0,color: Color(0xffC4C4C4),
+                                ),
+                                ),
                               ),
                             ),
                           ),
-                          RaisedButton(
-                            color: Color(0xffF1009C),
-                            onPressed: (){
-
-                            },
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10.0)
+                          Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(12.0),
+                                boxShadow:[BoxShadow(
+                                  color: Color(0xff6F29E6),
+                                  spreadRadius: 1.0,
+                                  blurRadius: 8.0,
+                                  offset: Offset(4,4),
+                                )]
                             ),
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Text('Sign up',style: TextStyle(
-                                fontSize: 20.0,color: Color(0xffC4C4C4),
+                            child: RaisedButton(
+                              color: Color(0xffF1009C),
+                              onPressed: (){
+                                Navigator.push(context, MaterialPageRoute(builder: (context)=>Signin()));
+                              },
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10.0)
                               ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text('Sign up',style: TextStyle(
+                                  fontSize: 20.0,color: Color(0xffC4C4C4),
+                                ),
+                                ),
                               ),
                             ),
                           )
